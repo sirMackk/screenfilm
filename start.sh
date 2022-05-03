68e054bdd259ce35985e3b9305870e4ed520d3c5
clark-hive <clark@thehive.ai>
2022-05-02 17:08:50 -0700

    retry each failure

--- start.sh
+++ start.sh
@@ -12,21 +12,29 @@ pushd "$dir" &>/dev/null || (echo "couldn't cd to $dir" &&  exit ) #absolute pat
 
 
 function already_running() {
-    pgrep -f "tracker.sh" | grep -cv 'grep\|vim\|nvim'
+    pgrep -f "$1" | grep -cv 'grep\|vim\|nvim'
 }
 
+## make paths absolute so can run in cron
 #commented out echos so don't keep getting mail
-if [[ $(already_running) -gt 0  ]]; then
-   #echo "tracker already running"
-   exit 0
+
+if [[ $(already_running "tracker.sh") -lt 1 ]]; then
+  #echo "Running stitcher"
+  /Users/clarkbenham/screenfilm/tracker.sh </dev/null &
+else 
+  #echo "stitcher already running"
+  :
+fi
+
+if [[ $(already_running "stitcher.sh") -lt 1  ]]; then
+# Wait for display to come up
+  sleep 2
+  #echo "Running tracker"
+  /Users/clarkbenham/screenfilm/stitcher.sh </dev/null & 
+  exit 0
+else
+  # echo "tracker already running"
+  :
 fi
 
 
-#echo "Running stitcher"
-## make paths absolute so can run in cron
-/Users/clarkbenham/screenfilm/stitcher.sh </dev/null & 
-
-# Wait for display to come up
-sleep 2
-#echo "Running tracker"
-/Users/clarkbenham/screenfilm/tracker.sh </dev/null &
